96fb4b9790d717c93335fea65f27f806bc98dddf
mk889426 <mk889426@gmail.com>
2024-12-11 15:44:03 +0530

    components added

---
+++ src/components/Question.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const Question = ({questionText}) => {
+    return (
+        <Box>
+            <h1>{questionText}</h1>
+        </Box>
+    )
+}
+
+export default Question
