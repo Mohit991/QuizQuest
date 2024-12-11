cbc2935381ebca2c3faa907e0d6041f151cfdbf9
Mohit <mohit94596@gmail.com>
2024-12-11 14:20:33 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/Homepage";
+import ChooseNoOfQuestions from "./pages/ChooseNoOfQuestions";
+import ChooseLevel from "./pages/ChooseLevel";
+import ChooseTopic from "./pages/ChooseTopic";
+import Quiz from "./pages/Quiz";
+import Question from "./components/Question";
+
+function App() {
+  return (
+    <>
+    {/* <Quiz /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/start"
+            element={<ChooseTopic />}
+          />
+          <Route
+            path="/start/:topic"
+            element={<ChooseNoOfQuestions />}
+          />
+          <Route
+            path="/start/:topic/:noOfQuestions"
+            element={<ChooseLevel />}
+          />
+          <Route
+            path="/start/:topic/:noOfQuestions/:level/quiz"
+            element={<Quiz />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
