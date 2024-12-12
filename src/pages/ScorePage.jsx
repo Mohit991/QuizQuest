008e205a76acbc67d8c5d4e13ab5db2ae36394be
Mohit <mohit94596@gmail.com>
2024-12-12 19:37:25 +0530

    Score page updated

--- src/pages/ScorePage.jsx
+++ src/pages/ScorePage.jsx
@@ -1,11 +1,16 @@
+import { Button } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 const ScorePage = () => {
     const location = useLocation();
     const { score } = location.state || { score: 0 }; // Get score 
 
+    const onQuizTryAgain = () => {
+        
+    }
     return(
         <>
             <h1>Your Score: {score}</h1>
+            <Button onClick={onQuizTryAgain}>Try Again</Button>
         </>
     )
 }
